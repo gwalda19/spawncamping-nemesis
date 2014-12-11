a55031488755839609855366b2a4ef2a8e49d61c
Mike Norris <michaelv.norris@gmail.com>
2014-12-11 11:54:23 -0500

    Added audio player.

---
+++ Phase3_Implementation/src/battlefieldAirmanPlayer/audio/AudioPlayerTest.java
@@ -0,0 +1,13 @@
+package battlefieldAirmanPlayer.audio;
+
+public class AudioPlayerTest {
+
+	public static void main(String[] args) {
+
+		String audioFilePath = "01.wav";
+		double percentage = 1;
+
+		AudioPlayer.playPercentage(audioFilePath, percentage);
+	}
+
+}
