26a90bdab732bd73f52d16875e2c31c70b4f679f
mnorr9 <michaelv.norris@gmail.com>
2014-12-14 22:40:16 -0500

    update - play audio from time in sec

--- Phase3_Implementation/src/battlefieldAirmanPlayer/audio/AudioPlayer.java
+++ Phase3_Implementation/src/battlefieldAirmanPlayer/audio/AudioPlayer.java
@@ -80,6 +80,60 @@ public class AudioPlayer implements LineListener {
         }
          
     }
+    
+    /**
+     * Play a given audio file.
+     * @param audioFilePath Path of the audio file.
+     */
+    private void playFromTime(String audioFilePath, double timeInSeconds) {
+        File audioFile = new File(audioFilePath);
+ 
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+ 
+            AudioFormat format = audioStream.getFormat();
+ 
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+ 
+            Clip audioClip = (Clip) AudioSystem.getLine(info);
+ 
+            audioClip.addLineListener(this);
+ 
+            audioClip.open(audioStream);
+                        
+            long duration = audioClip.getMicrosecondLength();// length in microseconds
+            System.out.println("Duration = " + (duration / 1000000.0) + " sec");
+            
+            long startTime = (long) (timeInSeconds*1000000.0);
+            System.out.println("Start Time: "+ (timeInSeconds) + " sec");
+            audioClip.setMicrosecondPosition(startTime);
+
+             
+            audioClip.start();
+             
+            while (!playCompleted) {
+                // wait for the playback completes
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+             
+            audioClip.close();
+             
+        } catch (UnsupportedAudioFileException ex) {
+            System.out.println("The specified audio file is not supported.");
+            ex.printStackTrace();
+        } catch (LineUnavailableException ex) {
+            System.out.println("Audio line for playing back is unavailable.");
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            System.out.println("Error playing the audio file.");
+            ex.printStackTrace();
+        }
+         
+    }
      
     /**
      * Listens to the START and STOP events of the audio line.
@@ -102,6 +156,11 @@ public class AudioPlayer implements LineListener {
     public static void playPercentage(String audioFilePath, double percentage) {
         AudioPlayer player = new AudioPlayer();
         player.play(audioFilePath, percentage);
+    }
+    
+    public static void playTime(String audioFilePath, double time) {
+        AudioPlayer player = new AudioPlayer();
+        player.playFromTime(audioFilePath, time);
     }
  
 }
--- Phase3_Implementation/src/battlefieldAirmanPlayer/audio/AudioPlayerTest.java
+++ Phase3_Implementation/src/battlefieldAirmanPlayer/audio/AudioPlayerTest.java
@@ -5,9 +5,13 @@ public class AudioPlayerTest {
 	public static void main(String[] args) {
 
 		String audioFilePath = "01.wav";
-		double percentage = 1;
+		//double percentage = 50;
 
-		AudioPlayer.playPercentage(audioFilePath, percentage);
+		//AudioPlayer.playPercentage(audioFilePath, percentage);
+		
+		double time = 115;
+
+		AudioPlayer.playTime(audioFilePath, time);
 	}
 
 }
